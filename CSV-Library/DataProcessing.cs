40efad133db12afb6bbee290dcc91c9e5942546a
Timofey Maksimov <tsmaksimov@edu.hse.ru>
2024-01-09 13:33:26 +0300

    Сделать первый коммит с игнорированными файлами

---
+++ CSV-Library/DataProcessing.cs
@@ -0,0 +1,340 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace CSV_Library
+{
+    public static class DataProcessing
+    {
+        /// <summary>
+        /// Поиск максимальной длины элемента для каждой выборки, чтобы позже выводить данные в читаемом табличном виде
+        /// </summary>
+        /// <param name="massivData">массив массивов</param>
+        /// <returns></returns>
+        public static int[] MaxLen(string[][] massivData)
+        {
+            int[] maxMassiv = new int[massivData[0].Length];
+            for (int i = 0; i < massivData[0].Length; i++)
+            {
+                for (int j = 0; j < massivData.Length; j++)
+                {
+                    if (maxMassiv[i] < massivData[j][i].Length)
+                    {
+                        maxMassiv[i] = massivData[j][i].Length;
+                    }
+                }
+            }
+            return maxMassiv;
+        }
+        /// <summary>
+        /// Поиск индекса с нужной выборкой
+        /// </summary>
+        /// <param name="massivData"></param>
+        /// <param name="var"></param>
+        public static int Search(string[][] massivData, string var)
+        {
+            int indexArea = -1;
+            // ищу номер элемента с такой выборкой
+            for (int i = 0; i < massivData[0].Length; i++)
+            {
+                if (massivData[0][i] == var)
+                {
+                    indexArea = i;
+                    break;
+                }
+            }
+            if (indexArea == -1)
+            {
+                Console.WriteLine("Ваш файл не соотвествует заявленным выборкам. Перезаргузите команду и выберите другой.");
+                indexArea = 0;
+            }
+            return indexArea;
+        }
+        /// <summary>
+        /// Вывод строки на экран
+        /// </summary>
+        public static void PrintString(string[][] massivData, int[] maxlen, int i)
+        {
+            for (int j = 0; j < massivData[i].Length; j++) // цикл для вывода массива
+            {
+                Console.Write($"{massivData[i][j]}");
+                // это цикл для пробелов, если в ячейке еще осталось место после вывода элемента
+                for (int f = 0; f < maxlen[j] - massivData[i][j].Length + 1; f++)
+                // +1 пробел так как хотя бы какой-то разделитель между элементами должен быть
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+        /// <summary>
+        /// Метод переводит массив элементов строки обратно в строку
+        /// </summary>
+        /// <param name="someString">Переданный массив элементов</param>
+        public static string MyToString(string[] someString)
+        {
+            StringBuilder stringNew = new StringBuilder("");
+            foreach (string el in someString)
+            {
+                stringNew.Append($"\"{el}\";");
+            }
+            stringNew.Append("\n");
+            return stringNew.ToString();
+        }
+        /// <summary>
+        /// Метод для вывода видоизмененных данных с помощью метода.
+        /// Преобразование массива массивов, в массив строк.
+        /// </summary>
+        /// <param name="massivData">массив всех данных</param>
+        /// <param name="indexValue">индекс значения</param>
+        /// <param name="value">значение по выборке</param>
+        /// <returns></returns>
+        public static string[] PrintAndGroup(string[][] massivData, int indexValue, string value)
+        {
+            // массив для вывода данных на экран. в нем лежит информация о размере ячейки для каждого элемента
+            // я ее ищу в методе, чтобы все данные выводились в табличном читаемом виде
+            int[] maxlen = MaxLen(massivData);
+
+            int kol = 0; // переменная для посчета строк, подходящих под выборку
+            for (int i = 2; i < massivData.Length; i++) // i=2, так как первые 2 строки отвечают за заголовки выборок
+            {
+                if (massivData[i][indexValue].Contains(value))
+                {
+
+                    PrintString(massivData, maxlen, i);
+                    kol++;
+                }
+            }
+
+            // массив и индекс для его заполнения
+            int indexSort; string[] sortMassiv;
+            // значит, мы точно передаем массив строк, то есть первые 2 нам нужны
+            if (kol > 1)
+            {
+                indexSort = 2;
+                sortMassiv = new string[kol+2];
+                sortMassiv[0] = MyToString(massivData[0]);
+                sortMassiv[1] = MyToString(massivData[1]);
+            }
+            // значит, мы передаем либо 0 строк, либо 1, то есть первые 2 нам не нужны
+            else {
+                indexSort = 0;
+                sortMassiv = new string[kol];
+            }
+            for (int i = 2; i < massivData.Length; i++) // i=2, так как первые 2 строки отвечают за заголовки выборок
+            {
+                if (massivData[i][indexValue].Contains(value))
+                {
+                    sortMassiv[indexSort] = MyToString(massivData[i]);
+                    indexSort++;
+                }
+            }
+            return sortMassiv;
+        }
+        /// <summary>
+        /// Поиск по выборке CoverageArea и вывод его на экран
+        /// </summary>
+        /// <param name="massivData"></param>
+        public static void CoverageArea(string[][] massivData)
+        {
+            int indexArea = Search(massivData, "CoverageArea");
+            Console.Write("Введите значение для данной выборки: ");
+            string area = Console.ReadLine();
+            while (area == null || area.Length == 0)
+            {
+                Console.Write("Введите значение не null и не пустое: ");
+                area = Console.ReadLine();
+            }
+
+            string[] sortMassiv = PrintAndGroup(massivData, indexArea, area);
+            if (sortMassiv.Length == 1) {
+                CsvProcessing.WriteFile(sortMassiv[0]);
+            }
+            if (sortMassiv.Length == 0)
+            {
+                Console.WriteLine("К сожалению, таких данных в базе нет :(");
+            }
+            if (sortMassiv.Length > 1)
+            {
+                CsvProcessing.WriteFile(sortMassiv);
+            }
+        }
+        /// <summary>
+        /// Поиск по выборке WifiName и вывод его на экран
+        /// </summary>
+        /// <param name="massivData"></param>
+        public static void WiFiName(string[][] massivData)
+        {
+            int indexWifiName = Search(massivData, "WiFiName");
+            Console.Write("Введите значение для данной выборки: ");
+            string wifiName = Console.ReadLine();
+            while (wifiName == null || wifiName.Length == 0)
+            {
+                Console.Write("Введите значение не null и не пустое: ");
+                wifiName = Console.ReadLine();
+            }
+
+            string[] sortMassiv = PrintAndGroup(massivData, indexWifiName, wifiName);
+            if (sortMassiv.Length == 1)
+            {
+                CsvProcessing.WriteFile(sortMassiv[0]);
+            }
+            if (sortMassiv.Length == 0)
+            {
+                Console.WriteLine("К сожалению, таких данных в базе нет :(");
+            }
+            if (sortMassiv.Length > 1)
+            {
+                CsvProcessing.WriteFile(sortMassiv);
+            }
+        }
+        /// <summary>
+        /// Поиск по выборке District и AccesFlag, а также вывод таких данных на экран
+        /// </summary>
+        /// <param name="massivData"></param>
+        public static void DistrictAndAccessFlag(string[][] massivData)
+        {
+            int indexDistrict = Search(massivData, "District");
+            Console.Write("Введите значение для выборки District: ");
+            string district = Console.ReadLine();
+            while (district == null || district.Length == 0)
+            {
+                Console.Write("Введите значение не null и не пустое: ");
+                district = Console.ReadLine();
+            }
+
+            int indexAccessFlag = Search(massivData, "AccessFlag");
+            Console.Write("Введите значение для выборки AccessFlag: ");
+            string accessFlag = Console.ReadLine();
+            while (accessFlag == null || accessFlag.Length == 0)
+            {
+                Console.Write("Введите значение не null и не пустое: ");
+                accessFlag = Console.ReadLine();
+            }
+
+            // массив для вывода данных на экран. в нем лежит информация о размере ячейки для каждого элемента
+            // я ее ищу в методе, чтобы все данные выводились в табличном читаемом виде
+            int[] maxlen = MaxLen(massivData);
+            // это прописал вызов метода для вывода первой строки, чтобы было понятно, где какие ячейки, но решил, что это не нужно
+            // PrintString(massivData, maxlen, 0);
+            int kol = 0; // переменная для посчета строк, подходящих под выборку
+            for (int i = 2; i < massivData.Length; i++) // i=2, так как первые 2 строки отвечают за заголовки выборок
+            {
+                if (massivData[i][indexDistrict].Contains(district) && massivData[i][indexAccessFlag].Contains(accessFlag))
+                {
+                    PrintString(massivData, maxlen, i);
+                    kol++;
+                }
+            }
+
+            // массив и индекс для его заполнения
+            int indexSort; string[] sortMassiv;
+            // значит, мы точно передаем массив строк, то есть первые 2 нам нужны
+            if (kol > 1)
+            {
+                indexSort = 2;
+                sortMassiv = new string[kol + 2];
+                sortMassiv[0] = MyToString(massivData[0]);
+                sortMassiv[1] = MyToString(massivData[1]);
+            }
+            // значит, мы передаем либо 0 строк, либо 1, то есть первые 2 нам не нужны
+            else
+            {
+                indexSort = 0;
+                sortMassiv = new string[kol];
+            }
+            for (int i = 2; i < massivData.Length; i++) // i=2, так как первые 2 строки отвечают за заголовки выборок
+            {
+                if (massivData[i][indexDistrict].Contains(district) && massivData[i][indexAccessFlag].Contains(accessFlag))
+                {
+                    sortMassiv[indexSort] = MyToString(massivData[i]);
+                    indexSort++;
+                }
+            }
+
+            if (sortMassiv.Length == 1)
+            {
+                CsvProcessing.WriteFile(sortMassiv[0]);
+            }
+            if (sortMassiv.Length == 0)
+            {
+                Console.WriteLine("К сожалению, таких данных в базе нет :(");
+            }
+            if (sortMassiv.Length > 1)
+            {
+                CsvProcessing.WriteFile(sortMassiv);
+            }
+        }
+        /// <summary>
+        /// Метод сортировки массива по значению CulturalCenterName (по алфавиту)
+        /// </summary>
+        /// <param name="massivData">массив массивов</param>
+        public static void CulturalCenterName(string[][] massivData)
+        {
+            int indexCulturalCenterName = Search(massivData, "CulturalCenterName");
+
+            for (int i = massivData.Length - 1; i > 2; i--)
+            {
+                for (int j = 2; j < i; j++)
+                {
+                    if (String.Compare(massivData[j][indexCulturalCenterName], massivData[j + 1][indexCulturalCenterName]) > 0)
+                    {
+                        string[] tmp = massivData[j][..]; // [..] для копирование значений, а не ссылок
+                        massivData[j] = massivData[j + 1][..];
+                        massivData[j + 1] = tmp[..];
+                    }
+                }
+            }
+            // массив для вывода данных на экран. в нем лежит информация о размере ячейки для каждого элемента
+            // я ее ищу в методе, чтобы все данные выводились в табличном читаемом виде
+            int[] maxlen = MaxLen(massivData);
+            for (int i = 2; i < massivData.Length; i++) // i=2, так как первые 2 строки отвечают за заголовки выборок
+            {
+                PrintString(massivData, maxlen, i);
+            }
+
+            // запись отсорированного массива массивов в массив строк
+            string[] sortMassiv = new string[massivData.Length];
+            for (int i = 0; i < massivData.Length; i++)
+            {
+                sortMassiv[i] = MyToString(massivData[i]);
+            }
+
+            CsvProcessing.WriteFile(sortMassiv);
+        }
+        /// <summary>
+        /// Метод сортировки массива по значению NumberOfAccessPoints (по возрастанию)
+        /// </summary>
+        /// <param name="massivData">массив массивов</param>
+        public static void NumberOfAccessPoints(string[][] massivData)
+        {
+            int indexNumberOfAccessPoints = Search(massivData, "NumberOfAccessPoints");
+            for (int i = massivData.Length - 1; i > 2; i--)
+            {
+                for (int j = 2; j < i; j++) // первые две строки не учитываем, так как они отвечают за названия выборок
+                {
+                    if (int.Parse(massivData[j][indexNumberOfAccessPoints]) > int.Parse(massivData[j + 1][indexNumberOfAccessPoints]))
+                    {
+                        string[] tmp = massivData[j][..]; // [..] для копирование значений, а не ссылок
+                        massivData[j] = massivData[j + 1][..]; 
+                        massivData[j + 1] = tmp[..];
+                    }
+                }
+            }
+            // массив для вывода данных на экран. в нем лежит информация о размере ячейки для каждого элемента
+            // я ее ищу в методе, чтобы все данные выводились в табличном читаемом виде
+            int[] maxlen = MaxLen(massivData);
+            for (int i = 2; i < massivData.Length; i++) // i=2, так как первые 2 строки отвечают за заголовки выборок
+            {
+                PrintString(massivData, maxlen, i);
+            }
+
+            string[] sortMassiv = new string[massivData.Length];
+            for (int i = 0; i < massivData.Length; i++)
+            {
+                sortMassiv[i] = MyToString(massivData[i]);
+            }
+
+            CsvProcessing.WriteFile(sortMassiv);
+        }
+    }
+}
---
+++ CSV-MainProgram/Program.cs
@@ -0,0 +1,124 @@
+﻿using System.Runtime.InteropServices;
+using System.Xml.Linq;
+
+
+namespace CSV_MainProgram
+{
+    // Максимов Тимофей Степанович, БПИ236-1, Вариант: 18
+
+    // Предисловие: так как файл мне был выдан, то я знаю разделитель данных, а также по первой строке могу высчитать
+    // количество выборок. В данном коде я не пользовался методом Split, так как некоторые данные в файле заключены в кавычках
+    // Для этого я написал отдельный метод, который позволяет сделать это без потери данных и исключений.
+    // В нем я знаю заранее количество выборок, а потому задаю его сразу - 16, дабы избежать перегруженности кода. 
+    // Если бы от меня конкретно требовалось также посчитать количество выборок, то делал я бы это с помощью первой строки
+    // отдельным методом. Просто не заполнял бы массив и не создавал, а лишь создал еще один метод, который был видоизменен с помощью
+    // вышеупомянутого метода только для подсчета выборок. Но так как я могу пользоваться выданным мне файлом, то я не создавал такой метод,
+    // чтобы не перегружать код. Хочется также упомянуть, что по первой строке я знаю, что строка завершается ";", а потому
+    // если в следующих строках в конце нет ";", то я считаю это за некорретную запись данных в файле и предупреждаю об этом пользователя 
+
+    // В КДЗ требуется выводить в табличном читаемом виде. По коду можно заметить, что я пытаюсь соблюсти этот параметр, однако
+    // размеры консоли не позволяет мне этого сделать, поэтому все равно выглядит немного криво, но получше, чем если совсем хаотично
+
+    // при сортировках пустые элементы выводятся вначале 
+
+    // C:\Users\lenovo\source\repos\KDZ-2\wifi-cult-centres.csv
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите абсолютный путь к файлу, у которого разделитель ';': ");
+            string[] result;
+            while (true)
+            {
+                try
+                {
+                    CSV_Library.CsvProcessing.FPath = Console.ReadLine(); // назначение пути
+                    result = CSV_Library.CsvProcessing.Read(); // массив строк
+                    Console.WriteLine("Файл успешно считан.");
+                    break;
+                }
+                catch (ArgumentNullException e)
+                {
+                    Console.WriteLine("Файл отсутствует или его структура не соответствуют варианту. Повторите попытку: ");
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine("Возникла ошибка при открытии файла, повторите попытку: ");
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine("Введено некорректное название файла, повторите попытку: ");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Возникла непредвиденная ошибка, повторите попытку: ");
+                }
+            }
+
+            string[][] massivData; // массив массивов с элементами по каждой строке
+            try
+            {
+                massivData = CSV_Library.CsvProcessing.SortOfInformation(result);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("Данные в файлe записаны неверно, начните программу заново и выберите другой файл.");
+                return;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Непредвиденная ошибка, пожалуйста, начните программу заново и выберите другой файл.");
+                return;
+            }
+
+            bool flag = true;
+            do
+            {
+                Console.WriteLine("Укажите номер пункта меню для запуска действия:");
+                Console.WriteLine("\t1. Произвести выборку по значению CoverageArea");
+                Console.WriteLine("\t2. Произвести выборку по значению WiFiName");
+                Console.WriteLine("\t3. Произвести выборку по значению District и AccessFlag");
+                Console.WriteLine("\t4. Отсортировать таблицу по значению CulturalCenterName (по алфавиту)");
+                Console.WriteLine("\t5. Отсортировать таблицу по значению NumberOfAccessPoints (по возрастанию)");
+                Console.WriteLine("\t6. Выйти из программы");
+                Console.WriteLine("\t7. Выбрать другой файл");
+                string numberOfPoint = Console.ReadLine();
+                switch (numberOfPoint)
+                {
+                    case "1":
+                        CSV_Library.DataProcessing.CoverageArea(massivData);
+                        break;
+                    case "2":
+                        CSV_Library.DataProcessing.WiFiName(massivData);
+                        break;
+                    case "3":
+                        CSV_Library.DataProcessing.DistrictAndAccessFlag(massivData);
+                        break;
+                    case "4":
+                        CSV_Library.DataProcessing.CulturalCenterName(massivData);
+                        break;
+                    case "5":
+                        try
+                        {
+                            CSV_Library.DataProcessing.NumberOfAccessPoints(massivData);
+                        }
+                        catch (Exception e)
+                        {
+                            // если там не число
+                            Console.WriteLine("Данные в файле записаны некорректны, выберите другой.");
+                        }
+                        break;
+                    case "6":
+                        flag = false;
+                        break;
+                    case "7":
+                        Main();
+                        return;
+                    default: 
+                        Console.WriteLine("Введенное значение может быть от 1 до 7, как выбор пункта для запуска действия, повторите попытку.");
+                        break;
+                }
+            } while (flag);
+        }
+    }
+}
